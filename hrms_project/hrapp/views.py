d8e61871e16d19d6b4fcef0a9b459cb9ef684680
SRINIVAS <srinivasacademics@gmail.com>
2025-06-18 13:28:50 +0530

    Initial commit

---
+++ hrms_project/hrapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Employee
+from .forms import EmployeeForm
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'hrapp/employee_list.html', {'employees': employees})
+
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')
+    else:
+        form = EmployeeForm()
+    return render(request, 'hrapp/add_employee.html', {'form': form})
+
+def edit_employee(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'hrapp/edit_employee.html', {'form': form})
+
+def delete_employee(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('employee_list')
+    return render(request, 'hrapp/delete_employee.html', {'employee': employee})
