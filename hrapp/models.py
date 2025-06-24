3cc19bb1b964a5fd2c70a22a0898f6c801f9ae38
SRINIVAS <srinivasacademics@gmail.com>
2025-06-24 15:59:20 +0530

    Initial commit

--- hrapp/models.py
+++ hrapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 
 
 class Employee(models.Model):
@@ -20,8 +20,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15)
     company_name = models.CharField(max_length=100, blank=True, null=True)
-    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)  # 👈 Add this line
-
+    #profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)  # 👈 Add this line
+    profile_image = CloudinaryField('image', blank=True, null=True) #cloudinary
     def __str__(self):
         return self.user.username
 class HRDetails(models.Model):
